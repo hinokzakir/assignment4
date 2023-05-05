054116a4b85c14ecf7d2faedda94a1a2ff3994d2
hinokzakir <114151045+hinokzakir@users.noreply.github.com>
2023-05-05 16:22:25 +0200

    1 file modified, 1 added, 1 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="navbar"></div>
-  <img src="" alt="Italian Trulli" />
-  <div class="footer"></div>
+  <!-- logo -->
+  <img
+    src="https://seeklogo.com/images/T/taxi-vehicle-sign-logo-04DAE3D598-seeklogo.com.png"
+    alt="logo"
+    style="width: 8rem; margin-top: 10px"
+  />
+
+  <div class="title">THE TAXI APP</div>
+  <!-- buttons -->
+  <div class="button-fb">Continue with facebook</div>
+  <div class="button-ios" style="margin-top: 1rem">Continue with Apple</div>
 </template>
 
 <script>
@@ -16,6 +25,28 @@ body {
   padding: 0;
   margin: 0;
 }
+.button-fb {
+  width: 15rem;
+  height: 3rem;
+  background-color: #1877f2;
+  color: white;
+  font-weight: bolder;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.button-ios {
+  width: 15rem;
+  height: 3rem;
+  background-color: black;
+  color: white;
+  font-weight: bolder;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .navbar {
   width: 100%;
   height: 5rem;
@@ -27,6 +58,11 @@ body {
   background-color: #b9f5d8;
   position: absolute;
   bottom: 0;
+}
+.title {
+  color: black;
+  font-size: 3rem;
+  font-weight: bolder;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
